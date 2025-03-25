6f7b7e2dc8fa24b7bfa8099efd0481f306c2c2f1
codinggolfer <eromon.agbomeirele@gmail.com>
2025-03-25 13:44:20 +0200

    nginx testing

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,6 +4,7 @@
 cp wp-config-sample.php wp-config.php
 
 # Replace DB settings using environment variables
+sed -i "s/localhost/$MYSQL_HOST/" wp-config.php
 sed -i "s/database_name_here/$MYSQL_DATABASE/" wp-config.php
 sed -i "s/username_here/$MYSQL_USER/" wp-config.php
 sed -i "s/password_here/$MYSQL_PASSWORD/" wp-config.php
