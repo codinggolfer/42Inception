3c8a09e8ae2c5b7177971e3dbd96050608880d6a
codinggolfer <eromon.agbomeirele@gmail.com>
2025-03-25 13:30:35 +0200

    nginx testing

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -8,6 +8,7 @@ sed -i "s/database_name_here/$MYSQL_DATABASE/" wp-config.php
 sed -i "s/username_here/$MYSQL_USER/" wp-config.php
 sed -i "s/password_here/$MYSQL_PASSWORD/" wp-config.php
 sed -i "s/localhost/$MYSQL_HOST/" wp-config.php
+sed -i 's|127.0.0.1:9000|0.0.0.0:9000|' /etc/php*/php-fpm.d/www.conf
 
 # Start PHP-FPM
 php-fpm83 -F
