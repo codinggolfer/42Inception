a07e0826e04be803444c784f773e498de3858e8c
codinggolfer <eromon.agbomeirele@gmail.com>
2025-03-26 14:36:36 +0200

    remodelling wordpress container

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -9,14 +9,15 @@
 set -e
 
 echo "Setting up database and users..."
-mysqld --bootstrap --datadir=/var/lib/mysql --user=mysql <<-EOF
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
+# mysqld --bootstrap --datadir=/var/lib/mysql --user=mysql <<-EOF
+# USE mysql;
+# FLUSH PRIVILEGES;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+# CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+# FLUSH PRIVILEGES;
+# EOF
 
 echo "Starting MariaDB"
 exec mysqld --user=mysql
